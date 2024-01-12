d909700ad7d5f1a9a6e147755d42c0cff45cf533
wiratama <dafa.wiratama@bankraya.co.id>
2024-01-12 21:54:32 +0700

    feat(weather): update API endpoint paths for getCity and getWeather functions
    The API endpoint paths for 'getCityApi' and 'getWeatherApi' functions have been updated to use 'direct1' and 'forecast1' respectively instead of 'direct' and 'forecast'. This change should provide more accurate weather data.

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -3,11 +3,11 @@ const key = process.env.REACT_APP_KEY;
 const host = process.env.REACT_APP_API;
 
 export const getCityApi = (city) => {
-  const url = `${host}/geo/1.0/direct?q=${city}&limit=5&appid=${key}`;
+  const url = `${host}/geo/1.0/direct1?q=${city}&limit=5&appid=${key}`;
   return axios.get(url);
 };
 
 export const getWeatherApi = (lat, lon) => {
-  const url = `${host}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
+  const url = `${host}/data/2.5/forecast1?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
   return axios.get(url);
 };
